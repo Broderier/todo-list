7d7f96c5d6df5cae05def37a11c4f92bf1a36b1e
Broderier <nnm.kb.90@gmail.com>
2022-01-16 00:55:41 +0900

    add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const port = 3000
+
+mongoose.connect('mongodb://localhost/todo-list')
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   app.send('Hello, world')

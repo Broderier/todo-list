be73947b3e5a29c49178ea0d32b6438be11e19fb
Broderier <nnm.kb.90@gmail.com>
2022-01-18 17:13:48 +0900

    feat: add create todo function

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const mongoose = require('mongoose')
 
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
 const Todo = require('./models/todo')
 
 mongoose.connect('mongodb://localhost/todo-list')
@@ -20,12 +22,30 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   // 拿到全部todo資料
   Todo.find()
     .lean()
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  // const todo = new Todo({ name })
+
+  // return todo.save()
+  //   .then(() => res.redirect('/'))
+  //   .catch(error => console.log(error))
+  return Todo.create({ name })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
